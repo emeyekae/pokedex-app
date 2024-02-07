e1cf58bb314f95815d7f3866b648cadc50eb8340
emeyekae <mkaleski@yahoo.com>
2024-02-07 17:09:36 -0500

    adding pokedex-app

---
+++ src/Pokecard.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Pokecard.css';
+
+const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+
+function Pokecard(props) {
+    let imgSrc = `${POKE_API}${props.id}.png`;
+  
+    return (
+        <div className="Pokecard">
+          <div className="Pokecard-name">{ props.name }</div>
+          <img className="Pokecard-image" src={imgSrc} />
+          <div className="Pokecard-type">Type: {props.type}</div>
+          <div className="Pokecard-exp">EXP: {props.exp}</div>
+        </div>
+    );
+  }
+  
+  export default Pokecard;
+  
